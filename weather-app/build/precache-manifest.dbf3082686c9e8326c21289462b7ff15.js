d3ee9bb7abff956018136f0b91d8e3c3ed983098
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-12-10 00:52:30 +0100

    Deployed website_2

---
+++ weather-app/build/precache-manifest.dbf3082686c9e8326c21289462b7ff15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb0f903094d2e97d697",
+    "url": "/weather-app-react/static/css/main.a910fb3f.chunk.css"
+  },
+  {
+    "revision": "afb0f903094d2e97d697",
+    "url": "/weather-app-react/static/js/main.afb0f903.chunk.js"
+  },
+  {
+    "revision": "a0e643d4d8d07acd9444",
+    "url": "/weather-app-react/static/js/1.a0e643d4.chunk.js"
+  },
+  {
+    "revision": "a69d6614ab32b008890e",
+    "url": "/weather-app-react/static/js/runtime~main.a69d6614.js"
+  },
+  {
+    "revision": "a4b597e643f353f8bc9bbfbfaf75263a",
+    "url": "/weather-app-react/index.html"
+  }
+];
